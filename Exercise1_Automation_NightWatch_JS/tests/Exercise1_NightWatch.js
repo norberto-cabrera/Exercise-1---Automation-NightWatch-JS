9081e661daf302687555b0569ca46de4244de546
Norberto Cabrera Alba <cabrera.norberto@hotmail.com>
2020-10-29 18:17:50 -0600

    Add files via upload

--- Exercise1_Automation_NightWatch_JS/tests/Exercise1_NightWatch.js
+++ Exercise1_Automation_NightWatch_JS/tests/Exercise1_NightWatch.js
@@ -3,11 +3,13 @@ module.exports = {
       browser
         //1.-Go to linkedin.com?originalSubdomain=mx
         .url     ('https://www.linkedin.com/?originalSubdomain=mx')
+        .maximizeWindow()
+        .pause(1000)
         .waitForElementVisible('body', 1000)
-        .pause(3000)
+        .pause(1000)
 
         //2.-Click on register. (The page has changed so I click on Join now)
-        .click('body:nth-child(2) nav.nav:nth-child(1) > a.nav__button-tertiary')
+        .click('body:nth-child(2) nav.nav:nth-child(1) div.nav__cta-container div.join-with-resume--flow-b > a.nav__button-tertiary.nav__button-tertiary--no-margin-right:nth-child(1)')
 
         //3.-Verify this text is present: Get started - it's free. (Make the most of your professional life)
         .assert.containsText("div.page.page--is-nux-design:nth-child(1) main.main form.join-form:nth-child(9) header.main__header > h1.main__subtitle.main__subtitle--is-cold-join","Make the most of your professional life")
